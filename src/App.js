a67b2442d1a90f9af224bac5979309e43f98e5d5
Kschoffner13 <koen.schoffner@ucalgary.ca>
2024-04-25 14:29:39 -0600

    router works just need to style for active and add background image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import logo from './logo.svg';
-import React from 'react';
-import Navbar from './components/Navbar';
-import './Styles/styles.css';
-// import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./Styles/styles.css";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Services from "./Pages/Services";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        
-        
-      </header>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,36 @@
 import "../Styles/styles.css";
 import RKSLogo from "../Images/RKSLogo.png";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <div>
-            
-            <nav className="navbar">
-               
-                <img src={RKSLogo} alt="RKS Logo" className="navbar-logo" />
-                <ul className="navbar-nav">
-                    <li>
-                        <a href="/" className="nav-link">
-                            Services
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/" className="nav-link">
-                            About us
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/" className="nav-link">
-                            Contact us
-                        </a>
-                    </li>
-
-                </ul>
-
-            
-            </nav>
-        </div>
-    )
+  return (
+    <div>
+      <nav className="navbar">
+        <img src={RKSLogo} alt="RKS Logo" className="navbar-logo" />
+        <ul className="navbar-nav">
+          <CustomLink to="/">Home</CustomLink>
+          <CustomLink to="/services">Services</CustomLink>
+          <CustomLink to="/about">About us</CustomLink>
+          <CustomLink to="/contact">Contact us</CustomLink>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: to, end: true });
+  console.log(resolvedPath.pathname);
+  console.log("isactive: ", isActive);
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
