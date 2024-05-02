aeb6fc6ef6770727858d4f7307eab80f32cd44c3
Kschoffner13 <koen.schoffner@ucalgary.ca>
2024-05-02 14:39:32 -0600

    added landing section to the home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,15 @@ import Footer from "../components/Footer";
 function Home() {
   return (
     <div className="HomeContent">
-      <div className="Welcome">
-        <h1>Welcome</h1>
-        <p>
-          welcome to rks calgary, we are a small business that specializes in
-          telecommuncations consulting.
-        </p>
+      <div className="landing">
+        <div className="L_content">
+          <h1 className="RKS_home">RKS</h1>
+          <h2>Consulting</h2>
+          <p>
+            Providing Quality Telecommunications and Software Consulting
+            Services Since 2024
+          </p>
+        </div>
       </div>
       <div className="intro"></div>
       <div className="services"></div>
